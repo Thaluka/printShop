0a2652b9e62e80ca414576fc3ec31d2b52ed40b4
unknown <w.g.thalu@gmail.com>
2021-04-17 08:27:19 +0530

    printshop backend files

---
+++ app/Http/Controllers/messageController.php
@@ -0,0 +1,99 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Response;
+use App\ModelS\Message;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendMail;
+
+class messageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "Hello";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      
+         $message = new Message;
+        $message->first_name = $request->input('first_name');
+        $message->last_name = $request->input('last_name');
+        $message->email = $request->input('email');
+        $message->subject = $request->input('subject');
+        $message->details = $request->input('description');
+        $message->save();
+        return Response::json([
+            'status' => 'Request Sent succeed..'
+        ], 200);
+        
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/template.blade.php
@@ -0,0 +1,4 @@
+<div>
+<p>{{ $data['id']}}Hi , This {{ $data['name'] }}</p>
+<p>I have some query like </P>
+</div>
